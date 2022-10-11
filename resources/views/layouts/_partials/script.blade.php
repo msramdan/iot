a8147ff157b17c1fc64034c492945fac304da223
Bayu29 <bsegara2903@gmail.com>
2022-10-11 16:24:43 +0700

    feat: halaman approve merchant

--- resources/views/layouts/_partials/script.blade.php
+++ resources/views/layouts/_partials/script.blade.php
@@ -1,7 +1,8 @@
 <script src=https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js></script>
 <script src=https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js></script>
 <script src=https://cdn.datatables.net/1.12.1/js/dataTables.bootstrap5.min.js></script>
-
+<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.4/moment.js" ></script>
+<script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 <script src="{{ asset('backend/assets/libs/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
 <script src="{{ asset('backend/assets/libs/simplebar/simplebar.min.js') }}"></script>
 <script src="{{ asset('backend/assets/libs/node-waves/waves.min.js') }}"></script>
