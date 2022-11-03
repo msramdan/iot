a33c78cbbe3e7aab024f3160cedfa55185e98231
Bayu29 <bsegara2903@gmail.com>
2022-11-03 13:57:26 +0700

    add chart

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Models\Transaction;
+use App\Models\Merchant;
+use App\Models\Kabkot;
 use Yajra\DataTables\Facades\DataTables;
 use RealRashid\SweetAlert\Facades\Alert;
 use Illuminate\Support\Facades\Validator;
@@ -10,6 +13,7 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Validation\Rules\Password;
 use Illuminate\Support\Facades\Hash;
 use Exception;
+use Carbon\Carbon;
 use App\Http\Controllers\Controller;
 
 class DashboardController extends Controller
@@ -19,9 +23,91 @@ class DashboardController extends Controller
         $this->middleware('auth');
     }
 
-    public function index()
+    public function index(Request $request)
     {
-        return view('admin.dashbaord.index');
+        //======================== Card Transaction ==================================
+        $start_date = explode(' ', Carbon::now()->toDateTimeString())[0]. ' 00:00:00';
+        $end_date = explode(' ', Carbon::now()->toDateTimeString())[0]. ' 23:59:59';
+
+        $start = $request->query('start') ? $request->query('start') : null;
+        $end = $request->query('end') ? $request->query('end') : null;
+
+        if ($start) {
+            $start = str_replace(',', '', $request->query('start')).' 00:00:00';
+
+            $start_date = date('Y-m-d H:i:s', strtotime($start));
+        }
+
+        if ($end) {
+            $end = str_replace(',', '', $request->query('end')).' 23:59:59';
+            $end_date = date('Y-m-d H:i:s', strtotime($end));
+        }
+
+        $transaction =  Transaction::whereBetween('created_at', [$start_date, $end_date])
+                                ->where('status', 'success');
+
+        $transaction_amount = $transaction->sum('amount');
+
+        $transaction_count = $transaction->count();
+
+        $total_fee_transaction = $transaction->sum('mdr_amount');
+
+        $total_merchant_active = Merchant::where('is_active', 1)->count();
+        //====================== End Card Transaction ==========================
+
+
+        //========================= Chart =============================
+        $year = $request->query('year') ? $request->query('year') : Carbon::now()->format('Y');
+
+        $transaction_month = Transaction::select(
+            DB::raw("DATE_FORMAT(created_at, '%m') as bulan"),
+            DB::raw("(COUNT(*)) as total_transaction"),
+            DB::raw("sum(amount) as total_amount")
+        )->where('status', 'success')
+         ->orderBy('created_at')
+         ->groupBy("bulan")
+         ->limit(10)
+         ->get();
+
+        $transaction_top_merchant = DB::table('transactions')
+                ->select(
+                    DB::raw('SUM(transactions.amount) as total_transaction'),
+                    DB::raw('merchants.merchant_name as merchant_name')
+                )
+                ->join('merchants', 'transactions.merchant_id', '=', 'merchants.id')
+                ->where('transactions.status', 'success')
+                ->groupBy('merchant_name')
+                ->orderBy('total_transaction', 'DESC')
+                ->limit(10)
+                ->get();
+
+        $merchant_active = DB::scalar(
+                    "SELECT (SELECT COUNT(merchants.id) WHERE merchants.is_active=0) AS total_inactive, (SELECT COUNT(merchants.id) WHERE merchants.is_active=1) AS total_active FROM `merchants` WHERE 1"
+                )->get();
+
+        $merchant_active = Merchant::where('is_active', 1)->count();
+        $merchant_inactive = Merchant::where('is_active', 0)->count();
+
+
+
+        // $transaction_top_merchant = Transaction::select(
+        //     DB::raw("sum(amount) as total_amount")
+        // )->where('status', 'success')
+        //  ->where(DB::raw("year(created_at) = {$year}"))
+        //  ->orderBy('total_amount')
+        //  ->groupBy('merchant_id')
+        //  ->limit(10)
+        //  ->get();
+
+        //  dd($transaction_top_merchant);
+        //======================= End Chart ===========================
+
+        return view('admin.dashbaord.index', compact(
+            'transaction_amount',
+            'transaction_count',
+            'total_fee_transaction',
+            'total_merchant_active'
+        ));
     }
 
     public function change_password(Request $request)
--- resources/views/admin/dashbaord/index.blade.php
+++ resources/views/admin/dashbaord/index.blade.php
@@ -1,4 +1,191 @@
 @extends('layouts.master')
 @section('title', 'Dashboard')
 @section('content')
+<div class="page-content">
+    <div class="container-fluid">
+
+        <!-- start page title -->
+        <div class="row">
+            <div class="col-12">
+                <div class="page-title-box d-sm-flex align-items-center justify-content-between">
+                    <h4 class="mb-sm-0">Dashboard</h4>
+
+                    <div class="page-title-right">
+                        <ol class="breadcrumb m-0">
+                            <li class="breadcrumb-item"><a href="javascript: void(0);">Dashboards</a></li>
+                            <li class="breadcrumb-item active">Dashboard</li>
+                        </ol>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        <!-- end page title -->
+
+        <div class="row">
+            <div class="col">
+
+                <div class="h-100">
+                    <div class="row mb-3 pb-1">
+                        <div class="col-12">
+                            <div class="d-flex align-items-lg-center flex-lg-row flex-column">
+                                <div class="flex-grow-1">
+                                    <h4 class="fs-16 mb-1">Good Morning, {{ Auth::user()->name }}</h4>
+                                </div>
+                                <div class="mt-3 mt-lg-0">
+                                    <form action="{{ route('dashboard') }}" method="get" id="filter_date">
+                                        @csrf
+                                        <div class="row g-3 mb-0 align-items-center">
+                                            <div class="col-sm-auto">
+                                                <div class="input-group">
+                                                    <input type="hidden" name="start">
+                                                    <input type="hidden" name="end">
+                                                    <input type="text" name="date" class="form-control border-0 dash-filter-picker shadow" data-provider="flatpickr" data-range-date="true" data-date-format="d M, Y" data-deafult-date="01 Jan 2022 to 31 Jan 2022">
+                                                    <div class="input-group-text bg-primary border-primary text-white">
+                                                        <i class="ri-calendar-2-line"></i>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <!--end row-->
+                                    </form>
+                                </div>
+                            </div><!-- end card header -->
+                        </div>
+                        <!--end col-->
+                    </div>
+                    <!--end row-->
+
+                    <div class="row">
+                        <div class="col-xl-3 col-md-6">
+                            <!-- card -->
+                            <div class="card card-animate">
+                                <div class="card-body">
+                                    <div class="d-flex align-items-center">
+                                        <div class="flex-grow-1 overflow-hidden">
+                                            <p class="text-uppercase fw-medium text-muted text-truncate mb-0">Today TRX Amount</p>
+                                        </div>
+                                    </div>
+                                    <div class="d-flex align-items-end justify-content-between mt-4">
+                                        <div>
+                                            <h4 class="fs-22 fw-semibold ff-secondary mb-4"><span class="counter-value" data-target="{{ $transaction_amount }}"></span></h4>
+                                        </div>
+                                        <div class="avatar-sm flex-shrink-0">
+                                            <span class="avatar-title bg-success rounded fs-3">
+                                                <i class="bx bx-dollar-circle"></i>
+                                            </span>
+                                        </div>
+                                    </div>
+                                </div><!-- end card body -->
+                            </div><!-- end card -->
+                        </div><!-- end col -->
+
+                        <div class="col-xl-3 col-md-6">
+                            <!-- card -->
+                            <div class="card card-animate">
+                                <div class="card-body">
+                                    <div class="d-flex align-items-center">
+                                        <div class="flex-grow-1 overflow-hidden">
+                                            <p class="text-uppercase fw-medium text-muted text-truncate mb-0">Today QTY Transactions</p>
+                                        </div>
+                                    </div>
+                                    <div class="d-flex align-items-end justify-content-between mt-4">
+                                        <div>
+                                            <h4 class="fs-22 fw-semibold ff-secondary mb-4"><span class="counter-value" data-target="{{ $transaction_count }}"></span></h4>
+                                        </div>
+                                        <div class="avatar-sm flex-shrink-0">
+                                            <span class="avatar-title bg-info rounded fs-3">
+                                                <i class="bx bx-shopping-bag"></i>
+                                            </span>
+                                        </div>
+                                    </div>
+                                </div><!-- end card body -->
+                            </div><!-- end card -->
+                        </div><!-- end col -->
+
+                        <div class="col-xl-3 col-md-6">
+                            <!-- card -->
+                            <div class="card card-animate">
+                                <div class="card-body">
+                                    <div class="d-flex align-items-center">
+                                        <div class="flex-grow-1 overflow-hidden">
+                                            <p class="text-uppercase fw-medium text-muted text-truncate mb-0">Total Indopay Fee AMT</p>
+                                        </div>
+                                    </div>
+                                    <div class="d-flex align-items-end justify-content-between mt-4">
+                                        <div>
+                                            <h4 class="fs-22 fw-semibold ff-secondary mb-4"><span class="counter-value" data-target="{{ $total_fee_transaction }}"></span></h4>
+                                        </div>
+                                        <div class="avatar-sm flex-shrink-0">
+                                            <span class="avatar-title bg-danger rounded fs-3">
+                                                <i class="bx bx-wallet"></i>
+                                            </span>
+                                        </div>
+                                    </div>
+                                </div><!-- end card body -->
+                            </div><!-- end card -->
+                        </div><!-- end col -->
+
+                        <div class="col-xl-3 col-md-6">
+                            <!-- card -->
+                            <div class="card card-animate">
+                                <div class="card-body">
+                                    <div class="d-flex align-items-center">
+                                        <div class="flex-grow-1 overflow-hidden">
+                                            <p class="text-uppercase fw-medium text-muted text-truncate mb-0">Total Active Merchants</p>
+                                        </div>
+                                    </div>
+                                    <div class="d-flex align-items-end justify-content-between mt-4">
+                                        <div>
+                                            <h4 class="fs-22 fw-semibold ff-secondary mb-4"><span class="counter-value" data-target="{{ $total_merchant_active }}"></span></h4>
+                                        </div>
+                                        <div class="avatar-sm flex-shrink-0">
+                                            <span class="avatar-title bg-warning rounded fs-3">
+                                                <i class="bx bx-user-circle"></i>
+                                            </span>
+                                        </div>
+                                    </div>
+                                </div><!-- end card body -->
+                            </div><!-- end card -->
+                        </div><!-- end col -->
+                    </div> <!-- end row-->
+
+                    <div class="row">
+                        <div class="col-xl-8">
+                            <div class="card">
+                                <div class="card-header p-0 border-0 bg-soft-light">
+                                </div><!-- end card header -->
+
+                                <div class="card-body p-0 pb-2">
+                                    <div class="w-100">
+                                        <div id="customer_impression_charts" data-colors='["--vz-success", "--vz-primary", "--vz-danger"]' class="apex-charts" dir="ltr"></div>
+                                    </div>
+                                </div><!-- end card body -->
+                            </div><!-- end card -->
+                        </div><!-- end col -->
+                    </div>
+
+                </div> <!-- end .h-100-->
+            </div> <!-- end col -->
+        </div>
+    </div>
+    <!-- container-fluid -->
+</div>
+<!-- End Page-content -->
 @endsection
+@push('js')
+<script>
+    $("input[name='date']").change(function() {
+       var dates = $(this).val();
+       var split_dates = dates.split(" to ");
+
+       console.log(split_dates.length);
+       if ( split_dates.length >= 2 ) {
+            $("input[name='start']").val(split_dates[0]);
+            $("input[name='end']").val(split_dates[1]);
+
+            $('#filter_date').submit();
+       }
+    })
+</script>
+@endpush
