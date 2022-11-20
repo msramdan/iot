ee08e0c72021789c68c0a34e2d2fa759784f6c7d
muhammad.ramdan <muhammad.ramdan@spesolution.com>
2022-11-21 06:11:19 +0700

    update

--- resources/views/admin/instance/_action.blade.php
+++ resources/views/admin/instance/_action.blade.php
@@ -1,12 +1,15 @@
 <td>
     @can('instance_update')
-    <a href="{{ route('instance.edit', $model->id) }}" class="btn btn-sm  btn-success"><i class="mdi mdi-pencil"></i>  </a>
+        <a href="{{ route('instance.edit', $model->id) }}" class="btn btn-sm  btn-success"><i class="mdi mdi-pencil"></i> </a>
     @endcan
     @can('instance_delete')
-    <form onsubmit="return confirm('Are you sure?');" action="{{ route('instance.destroy', $model->id) }}" method="POST" class="d-inline">
-        @csrf
-        @method('DELETE')
-        <button type="submit" class="btn btn-sm  btn-danger"><i class="mdi mdi-trash-can-outline"></i>  </button>
-    </form>
+        <form onsubmit="return confirm('Are you sure?');" action="{{ route('instance.destroy', $model->id) }}" method="POST"
+            class="d-inline">
+            @csrf
+            @method('DELETE')
+            <button type="submit" class="btn btn-sm  btn-danger"><i class="mdi mdi-trash-can-outline"></i> </button>
+        </form>
     @endcan
+    <a href="" class="btn btn-sm  btn-warning"><i class="mdi mdi-format-list-bulleted"></i>
+    </a>
 </td>
--- resources/views/admin/instance/index.blade.php
+++ resources/views/admin/instance/index.blade.php
@@ -1,69 +1,64 @@
 @extends('layouts.master')
 @section('title', 'Data Instance')
 @section('content')
-<div class="page-content">
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-12">
-                <div class="page-title-box d-sm-flex align-items-center justify-content-between">
-                    <h4 class="mb-sm-0">Instance</h4>
+    <div class="page-content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+                    <div class="page-title-box d-sm-flex align-items-center justify-content-between">
+                        <h4 class="mb-sm-0">Instance</h4>
 
-                    <div class="page-title-right">
-                        <ol class="breadcrumb m-0">
-                            <li class="breadcrumb-item"><a href="{{ route('dashboard') }}">Dashboard</a></li>
-                            <li class="breadcrumb-item active">Instance</li>
-                        </ol>
+                        <div class="page-title-right">
+                            <ol class="breadcrumb m-0">
+                                <li class="breadcrumb-item"><a href="{{ route('dashboard') }}">Dashboard</a></li>
+                                <li class="breadcrumb-item active">Instance</li>
+                            </ol>
+                        </div>
+
                     </div>
-
                 </div>
             </div>
-        </div>
-        <div class="row">
-            <div class="col-md-12">
-                <div class="card">
-                    <div class="card-header">
-                        @can('instance_create')
-                        <a href="{{ route('instance.create') }}" class="btn btn-md btn-secondary"> <i
-                                class="mdi mdi-plus"></i> Create</a>
-                        @endcan
-                    </div>
-                    <div class="card-body">
-                        <div class="table-responsive">
-                            <table class="table table-bordered table-sm" id="dataTable" style="width:100%">
-                                <thead>
-                                    <tr>
-                                        <th>#</th>
-                                        <th>Instance Code</th>
-                                        <th>Instance Name</th>
-                                        <th>Username</th>
-                                        <th>Bussiness</th>
-                                        <th>Email</th>
-                                        <th>Phone</th>
-                                        <th>Province</th>
-                                        <th>City</th>
-                                        <th>District</th>
-                                        <th>Village</th>
-                                        <th>Zip Code</th>
-                                        @canany(['instance_update', 'instance_delete'])
-                                        <th>Action</th>
-                                        @endcanany
-                                    </tr>
-                                </thead>
-                            </table>
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card">
+                        <div class="card-header">
+                            @can('instance_create')
+                                <a href="{{ route('instance.create') }}" class="btn btn-md btn-secondary"> <i
+                                        class="mdi mdi-plus"></i> Create</a>
+                            @endcan
+                        </div>
+                        <div class="card-body">
+                            <div class="table-responsive">
+                                <table class="table table-bordered table-sm" id="dataTable" style="width:100%">
+                                    <thead>
+                                        <tr>
+                                            <th>#</th>
+                                            <th>Instance Code</th>
+                                            <th>Instance Name</th>
+                                            <th>Bussiness</th>
+                                            <th>Email</th>
+                                            <th>Phone</th>
+                                            <th>City</th>
+                                            @canany(['instance_update', 'instance_delete'])
+                                                <th>Action</th>
+                                            @endcanany
+                                        </tr>
+                                    </thead>
+                                </table>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
 
+        </div>
     </div>
-</div>
 @endsection
 @push('js')
-<script>
-    const action = '{{ auth()->user()->can('instance_update') || auth()->user()->can('instance_delete') ? 'yes yes yes' : '' }}'
-        let columns = [
-            {
+    <script>
+        const action =
+            '{{ auth()->user()->can('instance_update') ||auth()->user()->can('instance_delete')? 'yes yes yes': '' }}'
+        let columns = [{
                 data: 'DT_RowIndex',
                 name: 'DT_RowIndex',
                 orderable: false,
@@ -78,10 +73,6 @@
                 name: 'instance_name'
             },
             {
-                data: 'username',
-                name: 'username',
-            },
-            {
                 data: 'bussiness',
                 name: 'bussiness',
             },
@@ -94,24 +85,8 @@
                 name: 'phone'
             },
             {
-                data: 'province',
-                name: 'province'
-            },
-            {
                 data: 'city',
                 name: 'city',
-            },
-            {
-                data: 'district',
-                name: 'district'
-            },
-            {
-                data: 'village',
-                name: 'village'
-            },
-            {
-                data: 'zip_code',
-                name: 'zip_code'
             }
         ]
 
@@ -130,5 +105,5 @@
             ajax: "{{ route('instance.index') }}",
             columns: columns
         });
-</script>
+    </script>
 @endpush
