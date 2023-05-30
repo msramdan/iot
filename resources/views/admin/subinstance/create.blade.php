fa0b5351b4c08ac3b33b14d7e060404d80696b3d
muhammad.ramdan <muhammad.ramdan@spesolution.com>
2023-05-30 08:10:37 +0700

    update

--- resources/views/admin/subinstance/create.blade.php
+++ resources/views/admin/subinstance/create.blade.php
@@ -49,7 +49,7 @@
                         <div class="col-md-12">
                             <div class="card">
                                 <div class="card-header">
-                                    <h3 class="card-title text-bold">Data Setting Device Alert Tollerance</h3>
+                                    <h3 class="card-title text-bold">Data Setting Device Alert Tolerance</h3>
                                 </div>
                                 <div class="card-body">
                                     <!-- Water Meter -->
