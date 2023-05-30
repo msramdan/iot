56c71ee5f0c0fb603ec00d43e6790e5b8f0275f3
muhammad.ramdan <muhammad.ramdan@spesolution.com>
2023-05-30 10:56:22 +0700

    update

--- database/migrations/2022_12_05_102955_create_devices_table.php
+++ database/migrations/2022_12_05_102955_create_devices_table.php
@@ -36,10 +36,10 @@ return new class extends Migration
             $table->string('supportClassC')->nullable();
             $table->string('macVersion')->nullable();
             $table->foreignId('cluster_id')->nullable();
-            $table->integer('is_error', 1)->nullable();
-            $table->timestamps();
+            $table->integer('is_error')->nullable();
             $table->foreign('subnet_id')->references('id')->on('subnets');
             $table->foreign('cluster_id')->references('id')->on('clusters');
+            $table->timestamps();
         });
     }
 
