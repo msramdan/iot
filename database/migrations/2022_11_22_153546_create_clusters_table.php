dccf7cc6d7edd292ba38a735680a1e5d1dc0e6e3
muhammad.ramdan <muhammad.ramdan@spesolution.com>
2023-06-21 13:44:50 +0700

    update

--- database/migrations/2022_11_22_153546_create_clusters_table.php
+++ database/migrations/2022_11_22_153546_create_clusters_table.php
@@ -19,12 +19,12 @@ return new class extends Migration
             $table->foreignId('subinstance_id');
             $table->string('kode');
             $table->string('name');
-            $table->float('xpercentage_water')->nullable();
-            $table->float('yfixed_cost_water')->nullable();
-            $table->float('xpercentage_power')->nullable();
-            $table->float('yfixed_cost_power')->nullable();
-            $table->float('xpercentage_gas')->nullable();
-            $table->float('yfixed_cost_gas')->nullable();
+            $table->float('xpercentage_water', 10, 4)->nullable();
+            $table->float('yfixed_cost_water', 10, 4)->nullable();
+            $table->float('xpercentage_power', 10, 4)->nullable();
+            $table->float('yfixed_cost_power', 10, 4)->nullable();
+            $table->float('xpercentage_gas', 10, 4)->nullable();
+            $table->float('yfixed_cost_gas', 10, 4)->nullable();
             $table->timestamps();
         });
     }
